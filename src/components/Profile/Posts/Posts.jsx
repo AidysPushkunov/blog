2e1c11177406705f1598afa8c0661e9e703983d2
Aidys <mywayinprogramming@gmail.com>
2022-05-06 20:03:40 +0700

    added posts

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -9,6 +9,8 @@ const Posts = () => {
         <div className={s.posts}>
             <Post image={'http://demiart.ru/forum/uploads5/post-203175-1263932175.jpg'} firstName="Azhu" lastName="Malov" titleText="JavaScript" postText={text} />
             <Post image={'https://vjoy.cc/wp-content/uploads/2020/10/da741321fed2fc603a92b5cb13101ecc.png'} firstName="Aisana" lastName="Barlova" titleText="C++" postText={text2} />
+            <Post image={'https://vjoy.cc/wp-content/uploads/2020/10/da741321fed2fc603a92b5cb13101ecc.png'} firstName="Aisana" lastName="Barlova" titleText="C++" postText={text2} />
+            <Post image={'https://vjoy.cc/wp-content/uploads/2020/10/da741321fed2fc603a92b5cb13101ecc.png'} firstName="Aisana" lastName="Barlova" titleText="C++" postText={text2} />
 
         </div>
     );
